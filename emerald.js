aa387889b095794bd60ee72e2b47730a1438ed42
macknatty <75257015+macknatty@users.noreply.github.com>
2023-12-13 16:25:06 +0300

    Add files via upload

---
+++ emerald.js
@@ -0,0 +1,96 @@
+/*-----------------------------------------adam------------------------------------------------------- */
+
+
+function chec() {
+  navigator.clipboard.writeText
+      ('https://retail.board.bluebez.com');
+}
+
+/*-----------------------------------------elias------------------------------------------------------- */
+           /* elias */
+  
+function checsc() {
+  navigator.clipboard.writeText
+      ('https://retail.board.bluebez.com');
+}
+/*------------------------------------------hab------------------------------------------------------ */
+
+     
+function bc() {
+  navigator.clipboard.writeText
+      ('https://retail.equal.bluebez.com');}
+
+     
+function cc() {
+  navigator.clipboard.writeText
+      ('https://retail-crown-bet.emerald-games.com');}
+
+
+     
+   function lec() {
+        navigator.clipboard.writeText
+            (' https://retail.hero.bluebez.com');}
+
+            
+
+      function iconc() {
+  navigator.clipboard.writeText
+      ('https://retail.fav.bluebez.com');
+
+
+
+}
+/*---------------------------------------muna--------------------------------------------------------- */
+
+  
+function holac() {
+  navigator.clipboard.writeText
+      ('https://retail-honda.emerald-games.com');
+
+}
+
+
+      function artc() {
+      navigator.clipboard.writeText
+      ('https://retail-art.emerald-games.com');
+}
+
+   
+function royalc() {
+  navigator.clipboard.writeText
+      ('https://retail-royal.emerald-games.com');
+}
+
+/*----------------------------------naty-------------------------------------------------------------- */
+
+
+
+function nicec() {
+  navigator.clipboard.writeText
+      ('https://retail.cool.bluebez.com');
+}
+
+function premierc() {
+  navigator.clipboard.writeText
+      ('https://retail.uk.bluebez.com');
+}
+
+/*----------------------------------------yeab------------------------------------------------------*/
+     
+function coffeec() {
+  navigator.clipboard.writeText
+      ('https://retail-coffee.emerald-games.com');
+}
+
+
+   
+function empirec() {
+  navigator.clipboard.writeText
+      ('https://retail-kingdom.emerald-games.com');
+}
+
+    
+function enjoyc() {
+  navigator.clipboard.writeText
+      ('https://retail-hat.emerald-games.com');
+}
